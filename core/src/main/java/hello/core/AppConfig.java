d328e9f2cd322fc85bb9bd5881334b9c3c425a01
qpwisu <qpwisu@gmail.com>
2024-07-21 23:10:05 +0900

    섹션[3] 20240721 완료

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,43 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderServiceImpl;
+import hello.core.order.OrderService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // CGLIB 상속 ( 실행중인 인스턴스면 재활용, 실행중이지 않으면 new 생성)
+public class AppConfig {
+
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    @Bean
+    public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("Call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
